d948bff2442961a7aadf01431b1f502819f46bd1
Your Name <you@example.com>
2023-03-16 04:09:38 +0900

    TOPページの追加

---
+++ resources/views/top.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<header>
+	<h1>Company Site</h1>
+	<nav>
+		<a href="/">トップ</a>
+	</nav>
+</header>
+<main>
+	<h1>これはトップページです</h1>
+</main>
+<footer>
+	&copy; 2020 XXXX
+</footer>
+</body>
+</html>

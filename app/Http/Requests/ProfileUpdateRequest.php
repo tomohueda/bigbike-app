095f2a4d4e96384dacb0d92d7242d9f67785d0db
Your Name <you@example.com>
2023-03-17 13:21:49 +0900

    管理者用Profileページの実装

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -15,6 +16,13 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
+        // 管理者orユーザーの判定
+        if ($this->is('admin/*')) {
+            return [
+                'name' => ['string', 'max:255'],
+                'email' => ['email', 'max:255', Rule::unique(Admin::class)->ignore($this->user()->id)],
+            ];
+        }
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,6 +38,11 @@ Route::prefix('admin')->name('admin.')->group(function(){
     Route::get('/dashboard', function () {
         return view('admin.dashboard');
     })->middleware('auth:admin')->name('dashboard');
+
+    // Admin用プロファイルページ
+    Route::get('/profile', [AdminProfileController::class, 'edit'])->middleware('auth:admin')->name('profile.edit');   
+    Route::patch('/profile', [AdminProfileController::class, 'update'])->middleware('auth:admin')->name('profile.update');
+    Route::delete('/profile', [AdminProfileController::class, 'destroy'])->middleware('auth:admin')->name('profile.destroy');
 
     require __DIR__.'/admin.php';
 });

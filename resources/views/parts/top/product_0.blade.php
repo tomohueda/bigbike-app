23e97ade5e3eebb0eab0b403eb6c9ae87d98c377
Tomoh Ueda <tomohueda@gmail.com>
2023-03-19 02:43:55 +0900

    商品コンテナ修正

--- resources/views/parts/top/product_0.blade.php
+++ resources/views/parts/top/product_0.blade.php
@@ -16,7 +16,9 @@
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>-->
+                <!--
                 <small class="text-muted">9 mins</small>
+                -->
               </div>
             </div>
             
